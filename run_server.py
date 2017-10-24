4a074d191d6c4625a0385069d08beef2fe9a7cbe
Hironsan <light.tree.1.13@gmail.com>
2017-10-24 09:47:44 +0900

    Update server

--- run_server.py
+++ run_server.py
@@ -2,6 +2,10 @@ import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+from tornado.options import define, options, parse_command_line
+
+
+define("port", default=5000, help="run on the given port", type=int)
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -10,11 +14,12 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 def main():
+    parse_command_line()
     application = tornado.web.Application([
         (r"/", MainHandler),
     ])
     http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.environ.get("PORT", 5000))
+    port = options.port
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
 
